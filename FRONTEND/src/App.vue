19aa5438ffca6af333e069f8e6e9e6494ba8ef4b
YoutscheenDerEchte <eugen.ruff@outlook.de>
2023-10-03 12:48:53 +0200

    modified:   FRONTEND/src/App.vue

--- FRONTEND/src/App.vue
+++ FRONTEND/src/App.vue
@@ -3,8 +3,8 @@
     <v-toolbar color="primary" density="compact" class="d-flex justify-start">
       <v-app-bar-title>TEILZEITPRÜFUNG</v-app-bar-title>
       <v-toolbar-items>
-        <router-link :to={ name: ÜbersichtView }><v-btn> Schulweise Übersicht</v-btn></router-link>
-        <router-link :to="{ name: PrüfungView}"><v-btn> Prüfung</v-btn></router-link>
+        <router-link :to="{ name: 'ÜbersichtView' }"><v-btn> Schulweise Übersicht</v-btn></router-link>
+        <router-link :to="{ name: 'PrüfungView' }"><v-btn> Prüfung</v-btn></router-link>
       </v-toolbar-items>
       <v-spacer></v-spacer>
       
