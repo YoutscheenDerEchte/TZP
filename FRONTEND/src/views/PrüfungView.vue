9a8aca0d8a5ffec5a15dd07da4785a256a20f69e
YoutscheenDerEchte <eugen.ruff@outlook.de>
2023-10-03 16:35:16 +0200

    modified:   "FRONTEND/src/views/Pr\303\274fungView.vue"

--- FRONTEND/src/views/PrüfungView.vue
+++ FRONTEND/src/views/PrüfungView.vue
@@ -1,11 +1,15 @@
 <template>
-    <v-sheet class="h-screen" :width="300" color="grey-darken-3">
-        
+    <v-sheet :style="{ height: `calc(100vh - ${toolbarHeight}px)` }" :width="300" color="grey-darken-3">
+        <v-combobox autofocus="true" label="Schulauswahl"
+            :items="schulen"></v-combobox>
     </v-sheet>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const toolbarHeight = 48
+const schulen = ref(['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming'])
 </script>
 
 <style lang="scss" scoped></style>
