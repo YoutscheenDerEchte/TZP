a4f3de9cd1f53889e72bf13c20bb745b4a86cda5
YoutscheenDerEchte <eugen.ruff@outlook.de>
2023-10-03 12:44:22 +0200

    modified:   FRONTEND/src/App.vue

--- FRONTEND/src/App.vue
+++ FRONTEND/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app>
-    <v-toolbar color="primary" density="compact">
+    <v-toolbar color="primary" density="compact" class="d-flex justify-start">
       <v-app-bar-title>TEILZEITPRÜFUNG</v-app-bar-title>
-      <v-toolbar-items class="float-left">
-        <v-btn outline color="secondary" dark>Prüfung</v-btn>
-        <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <router-link :to={ name: ÜbersichtView }><v-btn> Schulweise Übersicht</v-btn></router-link>
+        <router-link :to="{ name: PrüfungView}"><v-btn> Prüfung</v-btn></router-link>
       </v-toolbar-items>
+      <v-spacer></v-spacer>
       
     </v-toolbar>
     <router-view />
@@ -13,5 +14,7 @@
 </template>
 
 <script setup>
+import PrüfungView from './views/PrüfungView.vue';
+
 //
 </script>
