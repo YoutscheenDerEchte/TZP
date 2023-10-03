012d6e0eaaa5cbb55b26c06befd6a523b7839185
YoutscheenDerEchte <eugen.ruff@outlook.de>
2023-10-03 12:23:12 +0200

    modified:   FRONTEND/src/App.vue

--- FRONTEND/src/App.vue
+++ FRONTEND/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <v-app>
-    <v-app-bar density="compact">
+    <v-toolbar color="primary" density="compact">
       <v-app-bar-title>TEILZEITPRÜFUNG</v-app-bar-title>
-    </v-app-bar>
+      <v-toolbar-items class="float-left">
+        <v-btn outline color="secondary" dark>Prüfung</v-btn>
+        <v-spacer></v-spacer>
+      </v-toolbar-items>
+      
+    </v-toolbar>
     <router-view />
   </v-app>
 </template>
