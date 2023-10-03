c3324bb4a3b516b3bb67b59d4e34393e524a65a0
YoutscheenDerEchte <eugen.ruff@outlook.de>
2023-10-03 11:58:02 +0200

    modified:   FRONTEND/src/App.vue

--- FRONTEND/src/App.vue
+++ FRONTEND/src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-app-bar density="compact"></v-app-bar>
+  <v-app-bar density="compact">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Title</v-toolbar-title>
+  </v-app-bar>
   <router-view />
 </template>
 
 <script setup>
-  //
+//
 </script>
